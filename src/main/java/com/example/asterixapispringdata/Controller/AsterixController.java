f6400be5ad7aade68e55d33976dc0b0dd5dfdbe6
hinajawaid <hinajawaid.h@gmail.com>
2025-04-27 22:17:18 +0200

    Filter Added using Java methods for filtering)

--- src/main/java/com/example/asterixapispringdata/Controller/AsterixController.java
+++ src/main/java/com/example/asterixapispringdata/Controller/AsterixController.java
@@ -7,13 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import com.example.asterixapispringdata.Model.Character;
 
 @RestController
 public class AsterixController {
     // Declare the repository at the class level
-   private final CharacterService characterService;
-    private final CharacterRepository characterRepository;
+       private final CharacterService characterService;
+        private final CharacterRepository characterRepository;
 
     @Autowired
     public AsterixController(CharacterService characterService, CharacterRepository characterRepository) {
@@ -22,10 +24,24 @@ public class AsterixController {
     }
 
     //Get
-    @GetMapping("/asterix/characters")
+    /*@GetMapping("/asterix/characters")
     public List<Character> asterixCharacters() {
         // Declare the repository as an instance variable
         return characterService.getAllCharacters();
+    }
+     */
+    //Filter all characters based on the age parameter
+    @GetMapping("/asterix/characters")
+    public List<Character> getAllCharactersWithAgeFilter(@RequestParam(required = false) Integer age) {
+        List<Character> allCharacters = characterService.getAllCharacters();
+        if(age != null) {
+            return allCharacters.stream() //using stream
+                    .filter(character -> character.age() <= age) //Filter based on age, keep characters whose age is <= the provided age
+                    .collect(Collectors.toList());
+        }
+        else {
+            return characterService.getAllCharacters(); //return all character
+        }
     }
     //Create, Post //Take random generated Id. //from: @RequestBody Character newCharacter
     @PostMapping("/asterix/characters")
--- src/main/java/com/example/asterixapispringdata/Repository/CharacterRepository.java
+++ src/main/java/com/example/asterixapispringdata/Repository/CharacterRepository.java
@@ -3,5 +3,7 @@ package com.example.asterixapispringdata.Repository;
 import com.example.asterixapispringdata.Model.Character;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CharacterRepository extends MongoRepository<Character, String> {
 }
